649916ef3dc463cd81963918e078a56ca900024e
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2017-09-14 23:31:30 -0400

    Changing configs

--- src/constants/ServerConstants.java
+++ src/constants/ServerConstants.java
@@ -55,7 +55,7 @@ public class ServerConstants {
     public static final int MESO_RATE = 10;
     public static final int DROP_RATE = 10;
     public static final int BOSS_DROP_RATE = 25;
-    public static final int PARTY_EXPERIENCE_MOD = 2.5;           //Change for event stuff.
+    public static final int PARTY_EXPERIENCE_MOD = 2;           //Change for event stuff.
     public static final double EQUIP_EXPERIENCE_MOD = 10.0;     //Rate for equipment exp needed, grows linearly. Set 1.0 for default (about 100~200 same-level range mobs killed to pass equip from level 1 to 2).
     public static final double PQ_BONUS_EXP_MOD = 0.5;
     
