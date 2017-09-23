40284f5ada0f8383a7989619137fd1016c712f2a
Rayden <kevinxu.444@gmail.com>
2017-09-22 23:26:10 -0400

    henesys, kerning and ludi PQ level cap changed to 200

--- scripts/event/HenesysPQ.js
+++ scripts/event/HenesysPQ.js
@@ -5,7 +5,7 @@
 
 var isPq = true;
 var minPlayers = 3, maxPlayers = 6;
-var minLevel = 10, maxLevel = 255;
+var minLevel = 10, maxLevel = 200;
 var entryMap = 910010000;
 var exitMap = 910010300;
 var recruitMap = 100000200;
--- scripts/event/KerningPQ.js
+++ scripts/event/KerningPQ.js
@@ -5,7 +5,7 @@
 
 var isPq = true;
 var minPlayers = 3, maxPlayers = 4;
-var minLevel = 21, maxLevel = 30;
+var minLevel = 21, maxLevel = 200;
 var entryMap = 103000800;
 var exitMap = 103000890;
 var recruitMap = 103000000;
