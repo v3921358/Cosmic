e72052703b0d0ac2df23d8bb96c6fffcc3eb2f38
Rayden <kevinxu.444@gmail.com>
2017-09-26 18:32:53 -0400

    added search for pink/purple adventurer capes and gaia capes

--- sql/v1.02_db_changes/find_agent_e_items.sql
+++ sql/v1.02_db_changes/find_agent_e_items.sql
@@ -9,8 +9,6 @@ ii.itemid <> 1142107 AND ii.itemid <> 1142108
 ORDER BY cc.name ASC;
 
 
-
-
 SELECT inventoryitemid, itemid, characterid, name FROM inventoryitems AS ii
 INNER JOIN characters AS cc ON cc.id = ii.characterid
 WHERE
@@ -18,4 +16,11 @@ WHERE
 (ii.itemid IN (1012181, 1012182, 1012183, 1012184, 1012185, 1012186, 1012108, 1012109, 1012110, 1012111)) OR #face accessories
 (ii.itemid IN (1022073, 1022088, 1022103, 1022089)) OR #eye accessories
 (ii.itemid IN (1112407, 1112408, 1112401, 1112413, 1112414, 1112405, 1112402))) #rings
+ORDER BY cc.name ASC;
+
+
+# Pink and Purple Adventurer Cape and Gaia capes
+SELECT inventoryitemid, itemid, characterid, name FROM inventoryitems AS ii
+INNER JOIN characters AS cc ON cc.id = ii.characterid
+WHERE ii.itemid IN (1102041, 1102042, 1102021, 1102022, 1102023, 1102024, 1102084, 1102085, 1102086, 1102087)
 ORDER BY cc.name ASC;
