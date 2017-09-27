f35abdc490867fcd8593bcb6d377f9eef73f6713
Rayden <kevinxu.444@gmail.com>
2017-09-27 14:16:44 -0400

    changes to db drop_data

--- sql/v1.02_db_changes/arwen_glass_shoe.sql
+++
@@ -1,8 +0,0 @@
-/* 
-Quest: Arwen and the Glass Shoe (2017)
-Issue: Unable to complete quest due to Arwen's Glass Shoe not dropping from Fire Boars.
-*/
-
--- Adding Arwen's Glass Shoe (4001000) into drop table of Fire Boar (3210100)
-INSERT INTO maplesolaxia.drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
-VALUES (3210100, 4001000, 1, 1, 2017, 5000);
