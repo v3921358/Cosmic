1f62f94a1fe37320def899c2a0fbe9c943e78d31
Rayden <kevinxu.444@gmail.com>
2017-09-27 17:56:02 -0400

    moved Simon's db change into the correct patch version 1.02

--- sql/v1.02_db_changes/10200_missingdrops.sql
+++ sql/v1.02_db_changes/10200_missingdrops.sql
@@ -1,7 +1,7 @@
 INSERT INTO maplesolaxia.drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
 VALUES 
 (3210100, 4001000, 1, 1, 2017, 5000), -- FireBoar | Arwen's Glass Shoe | Arwen and the Glass Shoes
-(4300013, 4000538, 1, 1, 2288, 60000), -- Spirit of Rock | Spirit of Rock's Music Score | Say "NO" to Plagiarism
+(4300013, 4000538, 1, 1, 2288, 600000), -- Spirit of Rock | Spirit of Rock's Music Score | Say "NO" to Plagiarism
 (2130100, 4001367, 1, 1, 28257, 10000), -- Dark Axe Stump | Ripped Paper-1 | Who Broke the Seal of Andras?
 (1110100, 4001369, 1, 1, 28259, 10000), -- Green Mushroom | Ripped Paper-3 | Who Broke the Seal of Amdusias?
 (1210101, 4001370, 1, 1, 28260, 10000), -- Ribbon Pig | Ripped Paper-4 | Who Broke the Seal of Crocell?
