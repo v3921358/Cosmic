31aed43fcd2619bcc3f3dd482b32d9f34b6ea375
Rayden <kevinxu.444@gmail.com>
2017-10-01 19:14:46 -0400

    Fixed Jr. Boogie's drop data

--- sql/v1.03_db_changes/10300_missingdrops.sql
+++ sql/v1.03_db_changes/10300_missingdrops.sql
@@ -1,3 +1,13 @@
 INSERT INTO dietstory.drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
 VALUES 
 (3300003, 4001317, 1, 1, 2326, 5000); -- Helmet Pepe | Helmet Pepe's Helmet | James's Whereabouts (2)
+
+
+
+
+
+
+-- There are two Jr. Boogies mob ids for some unknown reason. 3230301 had no drops, but 3230300 had all the correct drops.
+-- Just copying the drops from the one with the correct drop data.
+INSERT INTO dietstory.drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
+SELECT 3230301, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance` FROM dietstory.drop_data WHERE dropperid = 3230300;
