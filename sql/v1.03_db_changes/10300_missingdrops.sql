82590a6299f27c541545eb4b3c9ca7898fb8333c
Rayden <kevinxu.444@gmail.com>
2017-10-04 14:50:19 -0400

    Added few missing drops in quest chain Unveiling the Map

--- sql/v1.03_db_changes/10300_missingdrops.sql
+++ sql/v1.03_db_changes/10300_missingdrops.sql
@@ -1,8 +1,10 @@
 INSERT INTO dietstory.drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
 VALUES 
-(3300003, 4001317, 1, 1, 2326, 5000); -- Helmet Pepe | Helmet Pepe's Helmet | James's Whereabouts (2)
-
-
+(3300003, 4001317, 1, 1, 2326, 5000), -- Helmet Pepe | Helmet Pepe's Helmet | James's Whereabouts (2)
+(1210103, 4032137, 1, 1, 20711, 5000), -- Bubbling | Bubbling Doll | Fake Doll 4032139
+(1210103, 4032139, 1, 1, 20713, 5000), -- Bubbling | Old Key | A Deal With Mr. Pickall
+(1210101, 4001357, 1, 1, 28244, 7000), -- Ribbon Pig | Pork | Grendel the Really Tipsy
+(1120100, 4001361, 1, 1, 28250, 7000); -- Octopus | Ink | The Mystery of the White Portion
 
 
 
@@ -10,4 +12,8 @@ VALUES
 -- There are two Jr. Boogies mob ids for some unknown reason. 3230301 had no drops, but 3230300 had all the correct drops.
 -- Just copying the drops from the one with the correct drop data.
 INSERT INTO dietstory.drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
-SELECT 3230301, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance` FROM dietstory.drop_data WHERE dropperid = 3230300;
+SELECT 3230301, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance` FROM dietstory.drop_data WHERE `dropperid` = 3230300;
+
+-- Wrong quest id for 'Calming the Soul with Music' quest.
+UPDATE dietstory.drop_data SET `questid` = 28248 WHERE `dropperid` = 130100 AND `itemid` = 4001358;
+UPDATE dietstory.drop_data SET `questid` = 28248 WHERE `dropperid` = 130101 AND `itemid` = 4001359;
