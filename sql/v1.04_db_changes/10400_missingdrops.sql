86a881a134f13a3c8510b8dffdbb1fba7a417b48
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2017-10-14 05:53:39 -0400

    Increased drop rate of peridots

--- sql/v1.04_db_changes/10400_missingdrops.sql
+++ sql/v1.04_db_changes/10400_missingdrops.sql
@@ -7,5 +7,5 @@ VALUES
 -- THIS IS FOR CYGNUS KNIGHTS, ONLY ADD TO drop_data IF PERIDOT IS ADDED TO THE WZ FILE! 
 INSERT INTO dietstory.drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
 VALUES 
-(8140001, 4032861, 1, 1, 20409, 80), -- Harp | Peridot | Empress' Grace
-(8140002, 4032861, 1, 1, 20409, 80); -- Blood Harp | Peridot | Empress' Grace
+(8140001, 4032861, 1, 1, 20409, 150), -- Harp | Peridot | Empress' Grace
+(8140002, 4032861, 1, 1, 20409, 150); -- Blood Harp | Peridot | Empress' Grace
