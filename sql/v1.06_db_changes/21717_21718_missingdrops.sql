85e48ce41111b7b127b71437e582060817784944
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2019-12-20 15:00:59 -0500

    [FIX] - drop data for quests 21711, 21718, 28205 (#23)
    * [FIX] - drop data for quests 21711, 21718, 28205
    
    * [FIX] - Updated Rowan's Request 1 to 21717
    
    * [FEAT] - Added droppable potions after every boss in Dojo

---
+++ sql/v1.06_db_changes/21717_21718_missingdrops.sql
@@ -0,0 +1,10 @@
+-- For Quest: Rowan's Request 1 - 21717
+INSERT INTO drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
+VALUES 
+(1110130, 4032317, 1, 1, 21717, 30000); -- Dejected Green Mushroom | Dejected Green Mushroom Puppet | Rowan's Request 1
+
+-- For Quest: Rowan's Request 2 - 21718
+INSERT INTO drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
+VALUES 
+(1110130, 4032318, 1, 1, 21718, 30000); -- Dejected Green Mushroom | Dejected Green Mushroom Puppet | Rowan's Request 2
+
---
+++ sql/v1.06_db_changes/28205_missingdrops.sql
@@ -0,0 +1,10 @@
+-- For Quest: Searching for the Lost Book - 28205
+INSERT INTO drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
+VALUES 
+(120100, 4001352, 1, 1, 28205, 30000), -- Shroom | Lost Book - Jay's lost book | Searching for the Lost Book
+(100101, 4001352, 1, 1, 28205, 30000), -- Blue Snail | Lost Book - Jay's lost book | Searching for the Lost Book
+(130101, 4001352, 1, 1, 28205, 30000), -- Red Snail | Lost Book - Jay's lost book | Searching for the Lost Book
+(1210100, 4001352, 1, 1, 28205, 30000), -- Pig | Lost Book - Jay's lost book | Searching for the Lost Book
+(1210101, 4001352, 1, 1, 28205, 30000), -- Ribbon Pig | Lost Book - Jay's lost book | Searching for the Lost Book
+(1210102, 4001352, 1, 1, 28205, 30000), -- Orange Mushroom | Lost Book - Jay's lost book | Searching for the Lost Book
+(1110100, 4001352, 1, 1, 28205, 30000); -- Green Mushroom | Lost Book - Jay's lost book | Searching for the Lost Book
