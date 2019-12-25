f57a045241bb8c3bc20851e5deb7d0cb9b0081e3
Yilun Zhao <yl4zhao@uwaterloo.ca>
2019-12-25 01:36:19 -0500

    Fix installation script

--- deploy/scripts/post_installation_setup.sh
+++ deploy/scripts/post_installation_setup.sh
@@ -2,7 +2,7 @@
 
 # Download configurations from S3
 if [ "$APPLICATION_NAME" == "dietstory-game-app-dev" ]; then
-	aws s3 sync s3://dietstory-api-server-assets/dev/config/dietstory_env.list ./
+	aws s3 cp s3://dietstory-api-server-assets/dev/config/dietstory_env.list dietstory_env.list
 elif [ "$APPLICATION_NAME" == "dietstory-game-app-prod" ]; then
-	aws s3 sync s3://dietstory-api-server-assets/prod/config/dietstory_env.list ./
+	aws s3 cp s3://dietstory-api-server-assets/prod/config/dietstory_env.list dietstory_env.list
 fi 
