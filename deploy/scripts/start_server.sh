013fcfc01fc5d3167be164ab9af926578972d3e8
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2020-01-06 02:58:16 -0500

    [FIX] Updating game server to consume larger weight of resources (#70)
    * [FIX] Updating game server to consume larger weight of resources
    
    * [CHORE] - environment dependent resources

--- deploy/scripts/start_server.sh
+++ deploy/scripts/start_server.sh
@@ -3,6 +3,14 @@
 SERVER_NAME=dietstory-game-server
 SERVER_PATH=/apps/dietstory-server
 
+if [ "$APPLICATION_NAME" == "dietstory-game-app-dev" ]; then
+	CPUS=1
+	CPU_SHARES=1024
+elif [ "$APPLICATION_NAME" == "dietstory-game-app-prod" ]; then
+	CPUS=2
+	CPU_SHARES=2048	
+fi
+
 source ~/.bashrc
 
 # Run Server
@@ -12,4 +20,6 @@ sudo docker run -it -d \
 	-e SERVER_HOST=${SERVER_HOST} \
 	--env-file ${SERVER_PATH}/dietstory_env.list \
 	--name=${SERVER_NAME} \
+	--cpus=${CPUS} \
+	--cpu-shares=${CPU_SHARES} \
 	benjixd/dietstory
