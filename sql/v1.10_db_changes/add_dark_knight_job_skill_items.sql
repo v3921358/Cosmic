b3d5da2b57cf6bada54d69bb7491d233b210d421
allen <iceyallen569@hotmail.com>
2020-01-25 22:13:10 -0500

    Added berzerk 20, 30, and berzerk job skill quest items

---
+++ sql/v1.10_db_changes/add_dark_knight_job_skill_items.sql
@@ -0,0 +1,4 @@
+insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (8500002,2290022, 1, 4, 0, 40000);
+insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (8800002,2290023, 1, 4, 0, 1080000);
+insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (8180001,4031479, 1, 1, 6151, 999999);
+insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (9300092,4031471, 1, 1, 6152, 40000);
