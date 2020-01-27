09cd45e4408a225bb6601f4d0e6fffaeef5b6f88
allen <iceyallen569@hotmail.com>
2020-01-26 21:58:43 -0500

    Added triple throw to papulatus and zakum drops as well, and comments to sql scripts

--- sql/v1.10_db_changes/add_dark_knight_job_skill_items.sql
+++ sql/v1.10_db_changes/add_dark_knight_job_skill_items.sql
@@ -1,4 +1,8 @@
+# Add Berserk 20 to papulatus drops
 insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (8500002,2290022, 1, 4, 0, 40000);
+# Add Berserk 30 zakum drops
 insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (8800002,2290023, 1, 4, 0, 1080000);
+# Add Sayram's necklace to Griffey's drop for Berserk Questline
 insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (8180001,4031479, 1, 1, 6151, 999999);
+# Add Sayram's shield to Jr balrog's drop for Berserk Questline
 insert ignore into drop_data (dropperid, itemid, minimum_quantity, maximum_quantity, questid, chance) values (9300092,4031471, 1, 1, 6152, 40000);
