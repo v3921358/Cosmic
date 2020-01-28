fa40143236cf3d15e4b887640e6bee58b776e85d
Benjamin Zhao <yl4zhao@uwaterloo.ca>
2020-01-27 20:06:47 -0500

    Fix/snipe quest (#110)
    * [FIXED] - NULL Pointer issue when trying to enter different dimension
    
    * beautify code
    
    * Fixing js typo

--- scripts/npc/1061012.js
+++ scripts/npc/1061012.js
@@ -3,62 +3,59 @@
 	Map(s): 		Dungeon : Another Entrance
 	Description: 		Takes you to another Dimension
 */
-
 function start() {
-    if (cm.getQuestStatus(6108) == 1) {
-	var ret = checkJob();
-	if (ret == -1) {
-	    cm.sendOk("Please form a party and talk to me again.");
-	} else if (ret == 0) {
-	    cm.sendOk("Please make sure that your party is a size of 2.");
-	} else if (ret == 1) {
-	    cm.sendOk("One of your party member's job is not eligible for entering the other world.");
-	} else if (ret == 2) {
-	    cm.sendOk("One of your party member's level is not eligible for entering the other world.");
+	if (cm.getQuestStatus(6108) == 1) {
+		var ret = checkJob();
+		if (ret == -1) {
+			cm.sendOk("Please form a party and talk to me again.");
+		} else if (ret == 0) {
+			cm.sendOk("Please make sure that your party is a size of 2.");
+		} else if (ret == 1) {
+			cm.sendOk("One of your party member's job is not eligible for entering the other world.");
+		} else if (ret == 2) {
+			cm.sendOk("One of your party member's level is not eligible for entering the other world.");
+		} else {
+			var em = cm.getEventManager("s4aWorld");
+			if (em == null) {
+				cm.sendOk("You're not allowed to enter with unknown reason. Try again.");
+			} else if (em.getProperty("started").equals("true")) {
+				cm.sendOk("Someone else is already attempting to defeat the Jr.Balrog in another world.");
+			} else {
+				var eli = em.getEligibleParty(cm.getParty());
+				if (eli.size() > 0 && !em.startInstance(cm.getParty(), cm.getMap())) {
+					cm.sendOk("A party in your name is already registered in this event.");
+				}
+			}
+		}
 	} else {
-	    var em = cm.getEventManager("s4aWorld");
-	    if (em == null) {
-		cm.sendOk("You're not allowed to enter with unknown reason. Try again." );
-	    } else if (em.getProperty("started").equals("true")) {
-		cm.sendOk("Someone else is already attempting to defeat the Jr.Balrog in another world." );
-	    } else {
-		if(!em.startInstance(cm.getParty(), cm.getMap())) {
-                    cm.sendOk("A party in your name is already registered in this event.");
-                }
-	    }
+		cm.sendOk("You're not allowed to enter the other world with unknown reason.");
 	}
-    }
-    else {
-        cm.sendOk("You're not allowed to enter the other world with unknown reason.");
-    }
-    
-    cm.dispose();
+
+	cm.dispose();
 }
 
-function action(mode, type, selection) {
-}
+function action(mode, type, selection) {}
 
 function checkJob() {
-    var party = cm.getParty();
+	var party = cm.getParty();
 
-    if (party == null) {
-	return -1;
-    }
-    //    if (party.getMembers().size() != 2) {
-    //	return 0;
-    //    }
-    var it = party.getMembers().iterator();
-
-    while (it.hasNext()) {
-	var cPlayer = it.next();
-
-	if (cPlayer.getJobId() == 312 || cPlayer.getJobId() == 322 || cPlayer.getJobId() == 900) {
-	    if (cPlayer.getLevel() < 120) {
-		return 2;
-	    }
-	} else {
-	    return 1;
+	if (party == null) {
+		return -1;
 	}
-    }
-    return 3;
+	
+	var it = party.getMembers().iterator();
+
+	while (it.hasNext()) {
+		var cPlayer = it.next();
+
+		if (cPlayer.getJobId() == 312 || cPlayer.getJobId() == 322 || cPlayer.getJobId() == 900) {
+			if (cPlayer.getLevel() < 120) {
+				return 2;
+			}
+		} else {
+			return 1;
+		}
+	}
+
+	return 3;
 }
