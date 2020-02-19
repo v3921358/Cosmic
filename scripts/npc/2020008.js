0641bd8e6b6deff8291722e59f49b02b2ec6a8ca
Yilun Zhao <yl4zhao@uwaterloo.ca>
2020-02-19 17:05:32 -0500

    fix - elnath pq entry

--- scripts/npc/2020008.js
+++ scripts/npc/2020008.js
@@ -21,23 +21,13 @@
 */
 
 status = -1;
+var minLevel = 120;
+var recruitMap = 211000001;
 var job;
 var sel;
 actionx = {"Mental" : false, "Physical" : false};
 
-function start() {
-    if(cm.isQuestStarted(6192)) {
-        if(cm.getWarpMap(921100300).getCharacters().size() > 0)
-            cm.sendOk("There is someone currently in this map, come back later.");
-        else {
-            cm.resetMapObjects(921100300);
-            cm.warp(921100300);
-        }
-            
-        cm.dispose();
-        return;
-    }
-    
+function start() {    
     if (!(cm.getPlayer().getLevel() >= 70 && parseInt(cm.getJobId() / 100) == 1)){
         cm.sendNext("Hi there.");
         cm.dispose();
@@ -47,7 +37,7 @@ function start() {
         actionx["Mental"] = true;
     else if (cm.haveItem(4031057))
         actionx["Physical"] = true;
-    cm.sendSimple("Can I help you?#b" + (cm.getJobId() % 10 == 0 ? "\r\n#L0#I want to make the 3th job advancement." : "") + "\r\n#L1#Please allow me to do the Zakum Dungeon Quest.");
+    cm.sendSimple("Can I help you?#b#L0#I want to make the 3th job advancement.\r\n#L1#Please allow me to do the Zakum Dungeon Quest.\r\nI would like to enter El Nath PQ.");
 }
 
 function action(mode, type, selection){
@@ -114,13 +104,54 @@ function action(mode, type, selection){
                 } else if (status == 2)
                     cm.sendNextPrev("The mental half of the test can only start after you pass the physical part of the test. #b#t4031057##k will be the proof that you have indeed passed the test. I'll let #b#p1022000##k in advance that you're making your way there, so get ready. It won't be easy, but I have the utmost faith in you. Good luck.");
             }
-        } else {
+            else {
+                cm.sendOk("Looks like you already have other commitments made. Come see me when you truly want to become a warrior.");
+                cm.dispose();
+            }
+        } else if(sel == 1){
             if (cm.getPlayer().getLevel() >= 50){
             	cm.sendNext("Ok, go.");
                 if(!cm.isQuestStarted(100200)) cm.startQuest(100200);
             }else
                 cm.sendNext("You're weak.");
             cm.dispose();
+        } else if(sel == 2) {
+            if(cm.getWarpMap(921100300).getCharacters().size() > 0)
+                cm.sendOk("There is someone currently in this map, come back later.");
+            else if(cm.getPlayer().getParty() == null || cm.getPlayer().getParty().getLeaderId() != cm.getPlayer().getId()) {
+                cm.sendOk("Please have your party leader speak to me.");
+            }
+            else if(!getPQRequirementsMet(cm.getPlayer().getParty())) {
+                cm.sendOk("Your party does not fulfill the minimum level requirement for this PQ.").
+            }
+            else {
+                cm.resetMapObjects(921100300);
+                cm.warpParty(921100300);
+            }
+            cm.dispose();
         }
     }
+}
+
+function getPQRequirementsMet(party) {      //selects, from the given party, the team that is allowed to attempt this event
+    var reqMet = false;
+    var hasLeader = false;
+    
+    if(party.size() > 0) {
+        var partyList = party.toArray();
+
+        for(var i = 0; i < party.size(); i++) {
+            var ch = partyList[i];
+
+            if(ch.getMapId() == recruitMap && ch.getLevel() >= minLevel) {
+                if(ch.isLeader()) hasLeader = true;
+                reqMet = true;
+            } else {
+                reqMet = false;
+                break;
+            }
+        }
+    }
+    
+    return reqMet && hasLeader;
 }
